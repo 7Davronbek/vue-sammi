5c04ab30d2e96c6c0c905966f006e895b68525d7
7Davronbek <maxmudovdavronbek8@gmail.com>
2024-01-01 20:44:54 +0500

    vue practise

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from '@/components/app/App.vue'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './styles/main.css'
-
-const app = createApp(App)
-
-app.mount('#app')
